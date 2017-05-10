7c81c0e15904dde6f0d05387a23d8f0ee9c152e2
Danilo Nascimento Cunha <id4ni10@gmail.com>
2017-05-10 20:46:04 +0000

    DHT11 + Soil + HTTP

--- thing/thing.py
+++ thing/thing.py
@@ -16,32 +16,33 @@ spi.open(0, 0)
 
 instance = dht11.DHT11(pin=14)
 
-def getAdc(channel):
+def getAdc(channel):	
         if channel > 7 or channel < 0:
                 return -1
 
         r = spi.xfer([1, 8 + channel << 4, 0])
         adcOut = ((r[1] & 3) << 8) + r[2]
         solo = int(round(adcOut / 10.24))
-        print 'ADC Output: {0:4d} Umidade do solo: {1:3}%'.format(adcOut, solo)
         sleep(0.1)
-        return solo
+        return 100 - solo
 
-def postData(temperatura, umidade, acelerometro, solo)        
-        data = { "Temperatura": temperatura, "Umidade": umidade, "Acelerometro": acelerometro, "Solo": solo }
+def postData(form):       
         req = urllib2.Request('https://homologacao.imap.org.br/prevent/api/store')
         req.add_header('Content-Type', 'application/json')
-        response = urllib2.urlopen(req, json.dumps(data))
+        response = urllib2.urlopen(req, json.dumps(form))
         
 while True:
-        solo = getAdc(5)
+        result = instance.read()                                
         
-        result = instance.read()        
-        temperature = result.is_valid() ? result.temperature : null        
-        humidity = result.is_valid() ? result.humidity : null                
-        
-        time.sleep(10000)
-        
-        postData(temperature, humidity, 0, solo)
-                
+	if result.is_valid():
+		solo = getAdc(5)
+
+	        data = { "Temperatura":	 result.temperature, "Umidade": result.humidity, "Acelerometro": 0, "Solo": solo }
+	
+		print(data) 
+	
+	        postData(data)
+
+	time.sleep(1)
+
 
