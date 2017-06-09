62a4780e2f255dae3248f37d87fccfb89d65566b
tiagosilva <tiago10.ba@hotmail.com>
2017-06-09 15:07:22 -0300

    Criação da class regra de negocio

---
+++ web/PreventCataclysmSystems/RegraNegocio/PrevencaoDeslizamento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PreventCataclysmSystems.RegraNegocio
+{
+    public class PrevencaoDeslizamento
+    {
+
+        public void DadosColetados(String tipoSolo, Int32 declividade, Int32 umidade, Int32 intencidade)
+        {
+        }
+
+        /*
+        * Segundo Castro (2003) uma das principais causas de tais deslizamentos é a infiltração de água e a embebição do solo das encostas. Os deslizamentos estão
+        * relacionados a três fatores: o tipo de solo, o declínio da encosta e a quantidade de água embebida no solo, que contribui para o aumento do peso da encosta.
+        * Castro (2003), cita que a medida local de níveis de embebição do terreno pela água, permite antecipar os desastres iminentes.
+        */
+
+        /*
+         * Variáveis necessárias:
+         * Tipo de Solo
+         * Declividade
+         * Umidade
+         * Intensidade da chuva
+         * Analise: "Não ocorreu deslizamento", "Ocorreu no segundo X, umidade: Y "
+         */
+
+        /*
+         * O tipo do solo será um parametro 
+         */
+    }
+}
